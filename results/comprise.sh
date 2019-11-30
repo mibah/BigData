7cde9db4adeb8e98aa41b3da9e5f5fd4a3fdaded
mibah <36843580+mibah@users.noreply.github.com>
2019-11-30 12:37:20 +0100

    Add files via upload

---
+++ results/comprise.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd collection/onlineAnalysisResults/AT
+cat *.csv > summary.csv
+cd ../../..
+
+cd collection/onlineAnalysisResults/BE
+cat *.csv > summary.csv
+cd ../../..
+
+cd collection/onlineAnalysisResults/CH
+cat *.csv > summary.csv
+cd ../../..
+
+cd collection/onlineAnalysisResults/DE
+cat *.csv > summary.csv
+cd ../../..
+
+cd collection/onlineAnalysisResults/FR
+cat *.csv > summary.csv
+cd ../../..
+
+cd collection/onlineAnalysisResults/LU
+cat *.csv > summary.csv
+cd ../../..
+
+cd collection/onlineAnalysisResults/NL
+cat *.csv > summary.csv
+cd ../../..
